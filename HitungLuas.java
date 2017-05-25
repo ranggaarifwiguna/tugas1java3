38e68f55ef3f0bc05851b20902c352f613f276fe
ranggaarifwiguna <rangga164@gmail.com>
2017-05-25 23:19:05 +0700

    Add files via upload

---
+++ HitungLuas.java
@@ -0,0 +1,12 @@
+package tugas1java3;
+
+public class HitungLuas {
+	
+	public int cariLuasPersegiPanjang(int panjang, int lebar){
+		return panjang * lebar;
+	}
+	
+	public double cariLuasLingkaran(int jarijari){
+		return Math.PI * jarijari * jarijari;
+	}
+}
